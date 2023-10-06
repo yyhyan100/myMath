cd5acc0959e33258db1916393e10c30c3ff8ef7f
yyhyan100 <yyhyan@gmail.com>
2023-10-06 08:11:58 -0500

    newton's

--- myMath/interpolation.py
+++ myMath/interpolation.py
@@ -14,3 +14,22 @@ class intp_1d:
         for i in range(len(x)):
             y_output += _Li(x,x_input,i)*y[i]
         return y_output
+
+    def NewtonDD(self,x,y,x_input):
+        n=len(x)
+        D=np.empty((n,n),dtype=np.float64)
+        D[:,0]=y
+
+        for j in range(1,n):
+            for i in range(j,n):
+                D[i,j]=(D[i,j-1]-D[i-1,j-1])/(x[i]-x[i-j])
+
+        y_output=0.0
+
+        for i in range(1,n):
+            tmp=1.0
+            for j in range(i):
+                tmp*=(x_input-x[j])
+            y_output+=D[i,i]*tmp
+        y_output+=D[0,0]
+        return y_output
