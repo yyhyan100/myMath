2977a951df575836626fadd2365a4b902bd38275
yyhyan100 <yyhyan@gmail.com>
2023-10-10 19:07:10 -0500

    hermite

--- myMath/interpolation.py
+++ myMath/interpolation.py
@@ -33,3 +33,19 @@ class intp_1d:
             y_output+=D[i,i]*tmp
         y_output+=D[0,0]
         return y_output
+    
+    def Hermite_2pts(self,x,y,yp,x_input):
+        tmp1=(x_input-x[0])/(x[1]-x[0])
+        tmp2=(x_input-x[1])/(x[0]-x[1])
+        return (1+2*tmp1)*tmp2**2*y[0]+(1+2*tmp2)*tmp1**2*y[1]+\
+             (x_input-x[0])*tmp2**2*yp[0]+(x_input-x[1])*tmp1**2*yp[1]
+    
+    def Hermite_3pts(self,x,y,yp,x_input):
+        tmp=-(x-x_input)
+        tp1=x[0]-x[1]
+        tp2=x[0]-x[2]
+        tp3=x[2]-x[1]
+        return tmp[1]**2*tmp[2]*y[0]/tp1**2/tp2+\
+            (1+tmp[1]/tp1+tmp[1]/tp3)*tmp[0]*tmp[2]*y[1]/tp1/tp3-\
+            tmp[0]*tmp[1]**2*y[2]/tp2/tp3**2+\
+            tmp[0]*tmp[1]*tmp[2]*yp/tp1/tp3
