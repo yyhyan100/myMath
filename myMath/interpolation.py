06c7f79c50fe385011ba4cc0748a6f3ed015ac42
yyhyan100 <yyhyan@gmail.com>
2023-10-27 13:05:35 -0500

    spline added

--- myMath/interpolation.py
+++ myMath/interpolation.py
@@ -50,31 +50,60 @@ class intp_1d:
             tmp[0]*tmp[1]**2*y[2]/tp2/tp3**2+\
             tmp[0]*tmp[1]*tmp[2]*yp/tp1/tp3
     
-    def spline(self, x, y, x_input):
+    def spline(self, x, y, x_input, bc=0):
         n=len(x)
-        gpp=np.zeros(n-2,dtype=np.float64)
+        gpp=np.zeros(n,dtype=np.float64)
         b=np.zeros(n-2,dtype=np.float64)
         A=np.zeros((n-2,n-2),dtype=np.float64)
-        for i in range(1,n-1):
-            delta_i=x[i+1]-x[i]
-            delta_i1=x[i+2]-x[i+1]
-            A[i,i-1]=delta_i/6
-            A[i,i]=(delta_i+delta_i1)/3
-            A[i,i+1]=delta_i1/6
-            b[i]=(y[i+2]-y[i+1])/delta_i1-(y[i+1]-y[i])/delta_i
+        for i in range(1,n-3):
+            d_im1=x[i+1]-x[i]
+            d_i=x[i+2]-x[i+1]
+            A[i,i-1]=d_im1/6
+            A[i,i]=(d_im1+d_i)/3
+            A[i,i+1]=d_i/6
+            b[i]=(y[i+2]-y[i+1])/d_i-(y[i+1]-y[i])/d_im1
+        if bc==0: # free run out f''(x0)=0
+            i=0
+            d_im1=x[i+1]-x[i]
+            d_i=x[i+2]-x[i+1]
+            A[i,i]=(d_im1+d_i)/3
+            A[i,i+1]=d_i/6
+            b[i]=(y[i+2]-y[i+1])/d_i-(y[i+1]-y[i])/d_im1
 
-        i=0
+            i=n-3
+            d_im1=x[i+1]-x[i]
+            d_i=x[i+2]-x[i+1]
+            A[i,i-1]=d_im1/6
+            A[i,i]=(d_im1+d_i)/3
+            b[i]=(y[i+2]-y[i+1])/d_i-(y[i+1]-y[i])/d_im1
+        else: # parabolic run out f''(x0)=f''(x1)
+            i=0
+            d_im1=x[i+1]-x[i]
+            d_i=x[i+2]-x[i+1]
+            A[i,i]=(d_im1+d_i)/3+d_im1/6
+            A[i,i+1]=d_i/6
+            b[i]=(y[i+2]-y[i+1])/d_i-(y[i+1]-y[i])/d_im1
+
+            i=n-3
+            d_im1=x[i+1]-x[i]
+            d_i=x[i+2]-x[i+1]
+            A[i,i-1]=d_im1/6
+            A[i,i]=(d_im1+d_i)/3+d_i/6
+            b[i]=(y[i+2]-y[i+1])/d_i-(y[i+1]-y[i])/d_im1
+
+        gpp[1:n-1]=np.linalg.solve(A,b)
+        if bc==0: # free run out f''(x0)=0
+            gpp[0]=0
+            gpp[n-1]=0
+        else:
+            gpp[0]=gpp[1]
+            gpp[n-1]=gpp[n-2]
+        for i in range(1,n):
+            if x_input<=x[i]: break
+        i-=1
+        tmp1=x_input-x[i]
+        tmp2=x[i+1]-x_input
         delta_i=x[i+1]-x[i]
-        delta_i1=x[i+2]-x[i+1]
-        A[i,i]=(delta_i+delta_i1)/3
-        A[i,i+1]=delta_i1/6
-        b[i]=(y[i+2]-y[i+1])/delta_i1-(y[i+1]-y[i])/delta_i
-
-        i=n-1
-        delta_i=x[i+1]-x[i]
-        delta_i1=x[i+2]-x[i+1]
-        A[i,i-1]=delta_i/6
-        A[i,i]=(delta_i+delta_i1)/3
-        b[i]=(y[i+2]-y[i+1])/delta_i1-(y[i+1]-y[i])/delta_i
-
-        gpp=np.matmul(np.linalg.inv(A),b.reshape(-1,0))
+        return gpp[i]*(tmp2**3/delta_i-delta_i*tmp2)/6+\
+            gpp[i+1]*(tmp1**3/delta_i-delta_i*tmp1)/6+\
+            y[i]*tmp2/delta_i+y[i+1]*tmp1/delta_i
