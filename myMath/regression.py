fae64141ff16bfde9c4b33d799d905535ff4dcea
yyhyan100 <yyhyan@gmail.com>
2024-01-30 20:24:15 -0600

    nonlinear regression

--- myMath/regression.py
+++ myMath/regression.py
@@ -52,3 +52,42 @@ class poly_regression:
     def predict(self,x_input):
         x=np.array([x_input**i for i in range(self.degree+1)])
         return np.sum(self.a*x)
+
+class exp_regression:
+# the model is y=a*exp(b*x), we need to find the coef a & b
+    def __init__(self):
+        self.a=None
+        self.b=None
+
+    def train(self,x,y):
+        lny=np.log(y) 
+        lr=linear_regression_single()
+        lr.train(x,lny)
+        self.a=exp(lr.a0)
+        self.b=lr.a1
+
+    def print_coef(self):
+        print(f"a={self.a},b={self.b}")
+
+    def predict(self,x_input):
+        return self.a*np.exp(self.b*x_input)
+
+class power_regression:
+# the model is y=a*x^b, we need to find the coef a & b
+    def __init__(self):
+        self.a=None
+        self.b=None
+
+    def train(self,x,y):
+        lnx=np.log(x)
+        lny=np.log(y) 
+        lr=linear_regression_single()
+        lr.train(lnx,lny)
+        self.a=exp(lr.a0)
+        self.b=lr.a1
+
+    def print_coef(self):
+        print(f"a={self.a},b={self.b}")
+
+    def predict(self,x_input):
+        return self.a*x_input**self.b
