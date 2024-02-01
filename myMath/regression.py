b240646eadf01f86127decddfa9de18f58868fa1
yyhyan100 <yyhyan@gmail.com>
2024-02-01 13:46:20 -0600

    mlr added

--- myMath/regression.py
+++ myMath/regression.py
@@ -23,7 +23,34 @@ class linear_regression_single:
     def predict(self,x_input):
         return self.a0+self.a1*x_input
         
-        
+class linear_regression_OLS:
+# multiple linear regression with OLS
+    def __init__(self):
+        self.w=None
+
+    def _preprocess(self,X):
+        n=X.shape[0]
+        tmp=np.ones((n,1))
+        return np.concatenate([tmp,X],axis=1)
+
+    def print_coef(self):
+        print(self.w)
+
+    def _ols(self,X,y):
+        return np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T,X)),X.T),y)
+
+    def train(self,X_train,y_train):
+        X=self._preprocess(X_train)
+        self.w=self._ols(X,y_train)
+
+    def predict(self,x_predict):
+        if len(x_predict.shape)==1:
+            X=np.concatenate([np.array([1]),x_predict])
+            return np.sum(self.w*X)
+        else:
+            X=self._preprocess(x_predict)
+            return np.matmul(X,self.w)
+
 class poly_regression:
 
     def __init__(self,degree:int=1):
